020eff6d89fde815c96661bc9e52b1f2b14784fe
mx-smblt <mx.smblt@gmail.com>
2024-02-24 11:45:30 +0200

    Added the new extension function `notNull`.

--- library/utils/src/main/kotlin/io/github/ustudiocompany/uframework/utils/NullableExtensions.kt
+++ library/utils/src/main/kotlin/io/github/ustudiocompany/uframework/utils/NullableExtensions.kt
@@ -1,6 +1,7 @@
 package io.github.ustudiocompany.uframework.utils
 
 import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
 
 @OptIn(ExperimentalContracts::class)
@@ -17,4 +18,13 @@ public fun <T> T?.isNotPresent(): Boolean {
         returns(false) implies (this@isNotPresent != null)
     }
     return this == null
+}
+
+@OptIn(ExperimentalContracts::class)
+public inline fun <T> T?.notNull(ifNull: () -> Nothing): T {
+    contract {
+        callsInPlace(ifNull, InvocationKind.AT_MOST_ONCE)
+        returns() implies (this@notNull != null)
+    }
+    return this ?: ifNull()
 }
