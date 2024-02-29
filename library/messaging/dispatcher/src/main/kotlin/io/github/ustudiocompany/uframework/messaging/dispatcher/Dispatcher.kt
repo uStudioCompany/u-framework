32f22dde4f54d5734bcd740142187450888a6145
mx-smblt <mx.smblt@gmail.com>
2024-02-29 21:35:32 +0200

    Changed the `Dispatcher` type.

--- library/messaging/dispatcher/src/main/kotlin/io/github/ustudiocompany/uframework/messaging/dispatcher/Dispatcher.kt
+++ library/messaging/dispatcher/src/main/kotlin/io/github/ustudiocompany/uframework/messaging/dispatcher/Dispatcher.kt
@@ -2,10 +2,11 @@
 
 package io.github.ustudiocompany.uframework.messaging.dispatcher
 
-import io.github.airflux.functional.getOrForward
+import io.github.airflux.functional.fold
 import io.github.ustudiocompany.uframework.failure.Failure
 import io.github.ustudiocompany.uframework.messaging.channel.deadletter.DeadLetterChannel
 import io.github.ustudiocompany.uframework.messaging.channel.deadletter.sendToDeadLetterChannel
+import io.github.ustudiocompany.uframework.messaging.dispatcher.Dispatcher.FailureHandler
 import io.github.ustudiocompany.uframework.messaging.handler.MessageHandler
 import io.github.ustudiocompany.uframework.messaging.message.IncomingMessage
 import io.github.ustudiocompany.uframework.messaging.router.Router
@@ -16,42 +17,64 @@ import io.github.ustudiocompany.uframework.telemetry.logging.diagnostic.context.
 import io.github.ustudiocompany.uframework.telemetry.logging.diagnostic.context.withDiagnosticContext
 
 context(Logging, DiagnosticContext)
-public fun <T> dispatcher(
-    deadLetterChannel: DeadLetterChannel<T>? = null,
-    router: RouterScope<T, MessageHandler<T>>.() -> Unit
-): Dispatcher<T> = Dispatcher(
-    deadLetterChannel = deadLetterChannel,
-    router = RouterScope<T, MessageHandler<T>>().apply(router).build()
+public fun <T, H : MessageHandler<T>> dispatcher(
+    failureHandler: FailureHandler<T>,
+    router: RouterScope<T, H>.() -> Unit
+): Dispatcher<T, H> = Dispatcher(
+    failureHandler = failureHandler,
+    router = RouterScope<T, H>().apply(router).build()
 )
 
-public class Dispatcher<T>(
-    private val deadLetterChannel: DeadLetterChannel<T>?,
-    private val router: Router<T, MessageHandler<T>>
+context(Logging, DiagnosticContext)
+public fun <T, H : MessageHandler<T>> dispatcher(
+    deadLetterChannel: DeadLetterChannel<T>,
+    router: RouterScope<T, H>.() -> Unit
+): Dispatcher<T, H> = dispatcher(failureHandler = FailureHandler.deadLetter(deadLetterChannel), router = router)
+
+public class Dispatcher<T, H : MessageHandler<T>>(
+    private val failureHandler: FailureHandler<T>,
+    private val router: Router<T, H>
 ) : MessageHandler<T> {
 
     context(Logging, DiagnosticContext)
     override fun handle(message: IncomingMessage<T>) {
-        val route = router.match(message)
-            .getOrForward { (failure) -> return handleFailure(failure, message) }
-        route.handler.handle(message)
+        router.match(message)
+            .fold(
+                onSuccess = { route -> route.handler.handle(message) },
+                onError = { failure -> failureHandler.handle(message, failure) }
+            )
     }
 
-    context(Logging, DiagnosticContext)
-    private fun handleFailure(failure: Failure, message: IncomingMessage<T>) {
-        if (deadLetterChannel != null)
-            message.sendToDeadLetterChannel(deadLetterChannel, ERROR_DESCRIPTOR, failure)
-        else
-            failure.logging()
-    }
+    public fun interface FailureHandler<T> {
 
-    context(Logging, DiagnosticContext)
-    private fun <F : Failure> F.logging() {
-        withDiagnosticContext(this) {
-            logger.error(getException()) { "$ERROR_DESCRIPTOR. ${joinDescriptions()}" }
+        context(Logging, DiagnosticContext)
+        public fun handle(message: IncomingMessage<T>, failure: Failure)
+
+        public companion object {
+
+            public val none: FailureHandler<Any> = FailureHandler { _, _ -> }
+
+            public val onlyLogging: FailureHandler<Any> =
+                FailureHandler { _, failure -> failure.logging() }
+
+            public fun <T> deadLetter(deadLetterChannel: DeadLetterChannel<T>): FailureHandler<T> =
+                FailureHandler { message, failure ->
+                    message.sendToDeadLetterChannel(deadLetterChannel, ERROR_DESCRIPTOR, failure)
+                }
+
+            private const val ERROR_DESCRIPTOR = "An error of a message router."
+
+            context(Logging, DiagnosticContext)
+            private fun Failure.logging(description: String? = null) {
+                withDiagnosticContext(this) {
+                    logger.error(getException()) {
+                        if (description != null)
+                            "$ERROR_DESCRIPTOR $description ${joinDescriptions()}"
+                        else
+                            "$ERROR_DESCRIPTOR ${joinDescriptions()}"
+                    }
+                }
+            }
         }
-    }
-
-    private companion object {
-        private const val ERROR_DESCRIPTOR = "An error of a message router."
     }
 }
