cd6ca82790fcf1cf90b90dba8382fd3550140fd4
mx-smblt <mx.smblt@gmail.com>
2024-03-03 10:39:27 +0200

    Changed the `SagaResolver#CommandResolverKey` type.

--- library/saga/engine/src/main/kotlin/io/github/ustudiocompany/uframework/saga/manager/SagaResolver.kt
+++ library/saga/engine/src/main/kotlin/io/github/ustudiocompany/uframework/saga/manager/SagaResolver.kt
@@ -8,30 +8,56 @@ import io.github.ustudiocompany.uframework.saga.internal.toMessageName
 import io.github.ustudiocompany.uframework.saga.internal.toMessageVersion
 
 public class SagaResolver private constructor(
-    private val resolverForCommand: Map<CommandKey, Saga<*>>,
+    private val resolverForCommand: Map<CommandResolverKey, Saga<*>>,
     private val resolverForSagaInstance: Map<SagaLabel, Saga<*>>
 ) {
 
     public fun resolve(name: MessageName, version: MessageVersion): Saga<*>? =
-        resolverForCommand[CommandKey(name = name, version = version)]
+        resolverForCommand[CommandResolverKey(name = name, version = version)]
 
     public fun resolve(label: SagaLabel): Saga<*>? = resolverForSagaInstance[label]
 
     public class Builder {
-        private val resolverForCommand = mutableMapOf<CommandKey, Saga<*>>()
+        private val resolverForCommand = mutableMapOf<CommandResolverKey, Saga<*>>()
         private val resolverForSagaInstance = mutableMapOf<SagaLabel, Saga<*>>()
 
         public fun <DATA> register(name: String, version: String, saga: Saga<DATA>): Unit =
             register(name.toMessageName(), version.toMessageVersion(), saga)
 
         public fun <DATA> register(name: MessageName, version: MessageVersion, saga: Saga<DATA>) {
-            val commandKey = CommandKey(name = name, version = version)
-            resolverForCommand[commandKey] = saga
-            resolverForSagaInstance[saga.definition.label] = saga
+            addToResolverForCommand(name, version, saga)
+            addToResolverForSagaInstance(saga)
         }
 
         public fun build(): SagaResolver = SagaResolver(resolverForCommand, resolverForSagaInstance)
+
+        private fun addToResolverForCommand(name: MessageName, version: MessageVersion, saga: Saga<*>) {
+            val key = CommandResolverKey(name, version)
+            resolverForCommand[key] = saga
+        }
+
+        private fun addToResolverForSagaInstance(saga: Saga<*>) {
+            resolverForSagaInstance[saga.definition.label] = saga
+        }
     }
 
-    private data class CommandKey(val name: MessageName, val version: MessageVersion)
+    private class CommandResolverKey(
+        val name: MessageName,
+        val version: MessageVersion
+    ) : Comparable<CommandResolverKey> {
+
+        override fun equals(other: Any?): Boolean =
+            this === other || (other is CommandResolverKey && this.name == other.name && this.version == other.version)
+
+        override fun hashCode(): Int {
+            var result = name.hashCode()
+            result = 31 * result + version.hashCode()
+            return result
+        }
+
+        override fun compareTo(other: CommandResolverKey): Int {
+            val byName = name.compareTo(other.name)
+            return if (byName != 0) byName else version.compareTo(other.version)
+        }
+    }
 }
