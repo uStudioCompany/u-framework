f415c43ea2635043bc617ba23e7e3bbd491fd5f8
mx-smblt <mx.smblt@gmail.com>
2024-03-20 17:03:24 +0200

    Updated version of the `kotlin-gradle-plugin`.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.4")
     implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.7.4")
     implementation("com.github.jk1:gradle-license-report:2.5")
