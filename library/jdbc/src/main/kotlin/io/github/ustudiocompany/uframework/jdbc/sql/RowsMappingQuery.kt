ba75a26d8c22c1cfa30845754365470e075db515
mx-smblt <mx.smblt@gmail.com>
2024-03-21 14:01:05 +0200

    Changed the `RowsMappingQuery` type.

--- library/jdbc/src/main/kotlin/io/github/ustudiocompany/uframework/jdbc/sql/RowsMappingQuery.kt
+++ library/jdbc/src/main/kotlin/io/github/ustudiocompany/uframework/jdbc/sql/RowsMappingQuery.kt
@@ -1,20 +1,41 @@
 package io.github.ustudiocompany.uframework.jdbc.sql
 
 import io.github.airflux.functional.Result
-import io.github.airflux.functional.flatMap
+import io.github.airflux.functional.error
+import io.github.airflux.functional.fold
+import io.github.airflux.functional.identity
 import io.github.airflux.functional.traverse
+import io.github.ustudiocompany.uframework.jdbc.error.ErrorConverter
 import io.github.ustudiocompany.uframework.jdbc.error.JDBCErrors
 import io.github.ustudiocompany.uframework.jdbc.row.Row
 import io.github.ustudiocompany.uframework.jdbc.sql.param.SqlParam
 import java.sql.Connection
 
-public class RowsMappingQuery<out T>(sql: ParametrizedSql, private val mapper: (Row) -> Result<T, JDBCErrors>) {
+public fun <T, F> rowsMappingQuery(
+    sql: ParametrizedSql,
+    errorConverter: ErrorConverter<F>,
+    mapper: (Row) -> Result<T, F>
+): RowsMappingQuery<T, F> = RowsMappingQuery(sql, errorConverter, mapper)
+
+public fun <T> rowsMappingQuery(
+    sql: ParametrizedSql,
+    mapper: (Row) -> Result<T, JDBCErrors>
+): RowsMappingQuery<T, JDBCErrors> = RowsMappingQuery(sql, ::identity, mapper)
+
+public class RowsMappingQuery<out T, out F>(
+    sql: ParametrizedSql,
+    private val errorConverter: ErrorConverter<F>,
+    private val mapper: (Row) -> Result<T, F>
+) {
     private val query = SqlQuery(sql)
 
-    public fun execute(connection: Connection, vararg parameters: SqlParam): Result<List<T>, JDBCErrors> =
+    public fun execute(connection: Connection, vararg parameters: SqlParam): Result<List<T>, F> =
         execute(connection, Iterable { parameters.iterator() })
 
-    public fun execute(connection: Connection, parameters: Iterable<SqlParam>): Result<List<T>, JDBCErrors> =
+    public fun execute(connection: Connection, parameters: Iterable<SqlParam>): Result<List<T>, F> =
         query.execute(connection, parameters)
-            .flatMap { it.traverse(mapper) }
+            .fold(
+                onSuccess = { rows -> rows.traverse(mapper) },
+                onError = { errorConverter(it).error() }
+            )
 }
