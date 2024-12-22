0181a77841fe8ac783a9f48574548df170a8458f
mx-smblt <mx.smblt@gmail.com>
2024-12-22 18:54:30 +0200

    Changed the `JDBCErrors` and the `Failure` types and the extension function `withTransaction` for the `Connection` type.

--- library/jdbc/src/main/kotlin/io/github/ustudiocompany/uframework/jdbc/Connection.kt
+++ library/jdbc/src/main/kotlin/io/github/ustudiocompany/uframework/jdbc/Connection.kt
@@ -1,13 +1,25 @@
 package io.github.ustudiocompany.uframework.jdbc
 
+import io.github.airflux.commons.types.identity
+import io.github.airflux.commons.types.resultk.ResultK
+import io.github.airflux.commons.types.resultk.asFailure
+import io.github.ustudiocompany.uframework.jdbc.error.ErrorConverter
+import io.github.ustudiocompany.uframework.jdbc.error.JDBCErrors
 import java.sql.Connection
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
 
+public inline fun <T> Connection.withTransaction(
+    block: Connection.() -> ResultK<T, JDBCErrors>
+): ResultK<T, JDBCErrors> = withTransaction(::identity, block)
+
 @OptIn(ExperimentalContracts::class)
 @Suppress("TooGenericExceptionCaught")
-public inline fun <T> Connection.withTransaction(block: Connection.() -> T): T {
+public inline fun <T, F> Connection.withTransaction(
+    errorConverter: ErrorConverter<F>,
+    block: Connection.() -> ResultK<T, F>
+): ResultK<T, F> {
     contract {
         callsInPlace(block, InvocationKind.EXACTLY_ONCE)
     }
@@ -23,6 +35,6 @@ public inline fun <T> Connection.withTransaction(block: Connection.() -> T): T {
         } catch (rollbackException: Throwable) {
             commitException.addSuppressed(rollbackException)
         }
-        throw commitException
+        errorConverter(JDBCErrors.UnexpectedError(commitException)).asFailure()
     }
 }
