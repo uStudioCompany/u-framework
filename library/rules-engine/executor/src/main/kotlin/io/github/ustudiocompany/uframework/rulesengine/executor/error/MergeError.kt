83dff23fef7211c7e5eb776ae0d92f66599fe642
mx-smblt <mx.smblt@gmail.com>
2024-12-27 22:29:45 +0200

    Changed

--- library/rules-engine/executor/src/main/kotlin/io/github/ustudiocompany/uframework/rulesengine/executor/error/MergeError.kt
+++ library/rules-engine/executor/src/main/kotlin/io/github/ustudiocompany/uframework/rulesengine/executor/error/MergeError.kt
@@ -4,6 +4,6 @@ import io.github.ustudiocompany.uframework.failure.Failure
 
 public class MergeError(cause: Failure) : RuleEngineError {
     override val code: String = "RULES-ENGINE-MERGE"
-    override val description: String = "Call error."
+    override val description: String = "Merging error."
     override val cause: Failure.Cause = Failure.Cause.Failure(cause)
 }
