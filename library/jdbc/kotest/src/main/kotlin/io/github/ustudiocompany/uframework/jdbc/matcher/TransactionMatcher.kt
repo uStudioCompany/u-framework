ee4ce4422d02d7a770459058945638524ed47aec
mx-smblt <mx.smblt@gmail.com>
2025-01-28 00:55:10 +0200

    Refactored the matchers for the `TransactionResult` type.

--- library/jdbc/kotest/src/main/kotlin/io/github/ustudiocompany/uframework/jdbc/matcher/TransactionMatcher.kt
+++ library/jdbc/kotest/src/main/kotlin/io/github/ustudiocompany/uframework/jdbc/matcher/TransactionMatcher.kt
@@ -1,15 +1,18 @@
 package io.github.ustudiocompany.uframework.jdbc.matcher
 
+import io.github.airflux.commons.types.AirfluxTypesExperimental
 import io.github.airflux.commons.types.fail.matcher.shouldContainErrorInstance
 import io.github.airflux.commons.types.fail.matcher.shouldContainExceptionInstance
 import io.github.airflux.commons.types.resultk.matcher.shouldContainFailureInstance
 import io.github.ustudiocompany.uframework.jdbc.error.JDBCError
 import io.github.ustudiocompany.uframework.jdbc.transaction.TransactionResult
 
-public fun <ErrorT : Any> TransactionResult<*, ErrorT>.shouldContainErrorInstance(): ErrorT =
+@OptIn(AirfluxTypesExperimental::class)
+public inline fun <reified ErrorT : Any> TransactionResult<*, ErrorT>.shouldContainErrorInstance(): ErrorT =
     shouldContainFailureInstance()
-        .shouldContainErrorInstance()
+        .shouldContainErrorInstance<ErrorT>()
 
+@OptIn(AirfluxTypesExperimental::class)
 public fun TransactionResult<*, *>.shouldContainExceptionInstance(): JDBCError =
     shouldContainFailureInstance()
         .shouldContainExceptionInstance()
