b1720069b46f174a84212f8d404fe677bf8c7afa
mx-smblt <mx.smblt@gmail.com>
2025-03-08 20:58:43 +0200

    Added the new methods for the `Details` type.

--- library/failure/src/main/kotlin/io/github/ustudiocompany/uframework/failure/Details.kt
+++ library/failure/src/main/kotlin/io/github/ustudiocompany/uframework/failure/Details.kt
@@ -1,20 +1,39 @@
 package io.github.ustudiocompany.uframework.failure
 
-public class Details private constructor(private val items: List<Item>) : List<Details.Item> by items {
+public class Details private constructor(private val items: MutableList<Item>) : List<Details.Item> by items {
+
+    /**
+     * Returns the value of the item from details by the specified [key] or null if the item is not found.
+     * @param key the key for search.
+     * @return the value or null.
+     */
+    public operator fun get(key: String): String? = items.find { it.key == key }?.value
 
     /**
      * Concatenates two details.
      * @param details the details to concatenate.
      * @return the new details.
      */
-    public operator fun plus(details: Details): Details = Details(this.items + details)
+    public operator fun plus(details: Details): Details = Details((this.items + details).toMutableList())
 
     /**
      * Adds the specified [item] to the details.
      * @param item the item to add.
      * @return the new details.
      */
-    public operator fun plus(item: Item): Details = Details(this.items + item)
+    public operator fun plus(item: Item): Details = Details((this.items + item).toMutableList())
+
+    public fun add(item: Pair<String, String>) {
+        add(Item(key = item.first, value = item.second))
+    }
+
+    public fun add(item: Item) {
+        items.add(item)
+    }
+
+    public fun add(details: Details) {
+        items.addAll(details)
+    }
 
     override fun toString(): String = items.joinToString(prefix = "[", postfix = "]") { it.toString() }
 
@@ -27,7 +46,7 @@ public class Details private constructor(private val items: List<Item>) : List<D
         /**
          * The empty details.
          */
-        public val NONE: Details = Details(emptyList())
+        public val NONE: Details = Details(mutableListOf())
 
         /**
          * Creates a new details with the specified [items].
@@ -49,6 +68,10 @@ public class Details private constructor(private val items: List<Item>) : List<D
          * @param items the items to create a new details.
          * @return the new details.
          */
-        public fun of(items: List<Item>): Details = if (items.isNotEmpty()) Details(items) else NONE
+        public fun of(items: List<Item>): Details =
+            if (items.isNotEmpty())
+                Details(mutableListOf<Item>().apply { addAll(items) })
+            else
+                NONE
     }
 }
