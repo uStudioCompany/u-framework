91a30ab6fdfb640b3b2872f923519e29093d0d76
mx-smblt <mx.smblt@gmail.com>
2025-05-15 14:48:56 +0300

    Removed unused code.
    Renamed and moved Path-related classes and tests to the new json-path module. Updated imports, dependencies, and configurations to reflect the migration. Ensured backward compatibility through adjustments in affected modules.

--- library/rules-engine/core/src/main/kotlin/io/github/ustudiocompany/uframework/rulesengine/core/data/Data.kt
+++
@@ -1,5 +0,0 @@
-package io.github.ustudiocompany.uframework.rulesengine.core.data
-
-import io.github.ustudiocompany.uframework.json.element.JsonElement
-
-public class Data(public val get: JsonElement)
