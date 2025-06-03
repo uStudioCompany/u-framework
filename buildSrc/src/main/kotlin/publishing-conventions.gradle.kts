20ae96777815d751add1c7c25df778a69cc3d865
mx-smblt <mx.smblt@gmail.com>
2025-06-03 21:21:40 +0300

    Changed configuration for the Gradle task (Sign)

--- buildSrc/src/main/kotlin/publishing-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/publishing-conventions.gradle.kts
@@ -3,12 +3,15 @@ plugins {
     signing
 }
 
+val signingKey: String? = System.getenv("GPG_PRIVATE_KEY")
+val signingKeyPassphrase: String? = System.getenv("GPG_PRIVATE_PASSWORD")
+
 configure<SigningExtension> {
-    val signingKey: String? = System.getenv("GPG_PRIVATE_KEY")
-    val signingKeyPassphrase: String? = System.getenv("GPG_PRIVATE_PASSWORD")
     useInMemoryPgpKeys(signingKey, signingKeyPassphrase)
 }
 
-tasks.withType<Sign>() {
-    onlyIf("this is a release build") { isReleaseBuild() }
+tasks.withType<Sign> {
+    onlyIf("this is a release build") {
+        isReleaseBuild() && signingKey != null && signingKeyPassphrase != null
+    }
 }
