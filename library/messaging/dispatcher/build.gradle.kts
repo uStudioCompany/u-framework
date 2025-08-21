e6310637953bdf6e33ec1ddd0ab88e5281468acc
Ruslan Burdeniuk <ruslan.burdeniuk@ustudio.company>
2025-08-21 17:33:17 +0300

    DRMCDU-977: update MessageHandler

--- library/messaging/dispatcher/build.gradle.kts
+++ library/messaging/dispatcher/build.gradle.kts
@@ -7,6 +7,8 @@ dependencies {
     implementation(project(":diagnostic-context-library"))
     implementation(project(":failure-library"))
     implementation(project(":logging-api-library"))
+    implementation(project(":logging-slf4j-library"))
+    implementation(project(":logging-formatter-json-library"))
     implementation(project(":messaging-core-library"))
     implementation(project(":messaging-dead-letter-channel-library"))
     implementation(project(":messaging-router-library"))
